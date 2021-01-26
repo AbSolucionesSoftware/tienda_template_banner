65494827d858685ac934b54672464aed26fc5ade
Diego <consultoresartificialbrain@gmail.com>
2021-01-26 15:50:59 -0600

    agregar todo

--- src/controllers/bannerCategoria.js
+++ src/controllers/bannerCategoria.js
@@ -14,7 +14,7 @@ bannerCtrl.subirImagen = async (req,res,next) => {
 
 bannerCtrl.getBanners = async (req,res) => {
     try {
-        const banners = await modelBanner.find({publicado: true});
+        const banners = await modelBanner.find({publicado: true}).sort({createdAt: -1});
         res.status(200).json(banners);
     } catch (error) {
         console.log(error);
